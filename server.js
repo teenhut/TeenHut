9ce75134fc1612597d870df8e6878798394880ee
Mukul7797 <mukulk3962364@gmail.com>
2025-12-04 01:20:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,17 @@ const handle = app.getRequestHandler();
 // Nodemailer Configuration
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000, // 10 seconds
+  debug: true, // show debug output
+  logger: true, // log information in console
 });
 
 // User Schema
